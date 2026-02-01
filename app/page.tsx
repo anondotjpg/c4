a0c41ac901f349af06d8e6923e6de7fd2126a6f3
agorapilled <johngardner13@outlook.com>
2026-02-01 02:07:13 -0500

    v0

--- app/page.tsx
+++ app/page.tsx
@@ -469,7 +469,12 @@ export default function BombDefusal() {
       display: "flex",
       alignItems: "center",
       justifyContent: "center",
-      position: "relative",
+      position: "fixed",
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      overflow: "auto",
     },
     mainLayout: {
       display: "flex",
